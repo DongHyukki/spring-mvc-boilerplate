d5fb8638559b9dd47f1b88e2498dfe14ba589704
DongHyukki <kderr2791@gmail.com>
2023-05-15 17:48:48 +0900

    feat: jpa, db 설정 업데이트

--- hyuki-app/build.gradle.kts
+++ hyuki-app/build.gradle.kts
@@ -5,6 +5,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-undertow")
     implementation("com.mysql:mysql-connector-j:8.0.32")
     implementation("com.atomikos:transactions-spring-boot-starter:5.0.9")
+
+    implementation("com.h2database:h2:2.1.214")
+    implementation("it.ozimov:embedded-redis:0.7.3") {
+        exclude(group = "org.slf4j", module = "slf4j-api")
+        exclude(group = "org.slf4j", module = "slf4j-simple")
+    }
+    implementation("mysql:mysql-connector-java:8.0.32")
+
 }
 tasks.getByName("bootJar") {
     enabled = true
